767b946b2861d27a421a0fe5441adcf1ac8a7228
Exia4117 <593663174.bz@gmail.com>
2018-10-25 21:02:36 +0800

    check network

--- app/src/main/java/com/example/a59366/weatherreport/MainActivity.java
+++ app/src/main/java/com/example/a59366/weatherreport/MainActivity.java
@@ -2,11 +2,24 @@ package com.example.a59366.weatherreport;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.a59366.weatherreport.com.example.a59366.util.NetUtil;
 
 public class MainActivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.weather_info);
+//    private static final android.util.Log Log = ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络OK！", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
         }
+    }
 }
